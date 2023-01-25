7a555f59bb237fa55f14474f63b39d2b6206c655
sfjbs <urmhaskar2000@gmail.com>
2023-01-25 13:48:24 +0000

    Dockerisation

--- Day08/student-app/src/test/java/com/upasana/app/StudentTest.java
+++ Day08/student-app/src/test/java/com/upasana/app/StudentTest.java
@@ -2,6 +2,7 @@ package com.upasana.app;
 
 import com.upasana.app.model.Student;
 import org.junit.jupiter.api.Test;
+import org.junit.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +11,17 @@ public class StudentTest {
     @Test
     public void getStudentTest(){
 
-//        List listStudent = new ArrayList();
-//        Student student1 =
+        List listStudent = new ArrayList();
+        Student student1 = new Student("ABC", "001", "5th", "PLS");
+        Student student2 = new Student("DEF","002", "6th", "JBS");
+        Student student3= new Student("GHI","003","7th","DGS");
+
+        listStudent.add(student1);
+        listStudent.add(student2);
+        listStudent.add(student3);
+        Assert.assertEquals(listStudent.size(),3);
+
+
 
     }
 }
